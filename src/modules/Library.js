38ddb1b2c5f5c64e0a75984ca79427a8287ab6df
alopine <loariel98@gmail.com>
2022-05-20 12:03:39 -0700

    Create Library class

---
+++ src/modules/Library.js
@@ -0,0 +1,33 @@
+import Book from './Book';
+
+export default class Library {
+  constructor() {
+    this.allBooks = [new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'Not Read')];
+  }
+
+  getAllBooks() {
+    return this.allBooks;
+  }
+
+  setAllBooks(arg) {
+    if (arg) {
+      this.allBooks = arg;
+    }
+  }
+
+  addBook(argBook) {
+    if (argBook) {
+      this.allBooks.push(argBook);
+    }
+  }
+
+  deleteBook(argID) {
+    this.allBooks = this.allBooks.filter(
+      (book) => book.id !== argID,
+    );
+  }
+
+  findBook(argID) {
+    return this.allBooks.find((book) => book.id === argID);
+  }
+}
